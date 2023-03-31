370c796d3f7fc4854fb52757a7ae73a1a98b3dac
Aidan <daultimatepotato@gmail.com>
2023-03-31 13:42:17 -0500

    updated gitignore

--- src/CSC4101Assignment2/ConvertorTest.java
+++ src/CSC4101Assignment2/ConvertorTest.java
@@ -7,6 +7,6 @@ package CSC4101Assignment2;
  */
 public class ConvertorTest {
     public static void main (String args[]){
-        Convertor.IEEE754(12315.241414);
+        Convertor.IEEE754(-12315.241414);
     }
 }
